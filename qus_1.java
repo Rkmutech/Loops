197c5d47800cec3c99ca7021f850b2298278d5e8
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-04 11:48:37 +0530

    Add first question

---
+++ qus_1.java
@@ -0,0 +1,21 @@
+public class qus_1 {
+    public static void print_n_numbers(int n){
+        for(int i=1;i<n;i++)
+        {
+            System.out.println(i);
+        }
+    
+    
+    }
+        public static void main(String[] args) {
+            print_n_numbers(5);
+            System.out.println("Next input");
+            print_n_numbers(10);
+            System.out.println("Next input");
+            print_n_numbers(14);
+            System.out.println("Next input");
+            print_n_numbers(100);
+    
+        }
+    
+}
