8b94da9b325521eb9b0ed5ecbbdaff7eba283859
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-04 13:07:00 +0530

    add the question

--- question_8/qus_8.java
+++ question_8/qus_8.java
@@ -1,7 +1,17 @@
 package question_8;
+/* display multiplication upto 20 for given number  */
 
 public class qus_8 {
+    public static void mutiplication_table_upto_20(int n)
+    {
+        for(int i=1;i<=20;i++)
+        {
+            System.out.println("1X"=);
+        }
+    }
+    
     public static void main(String[] args) {
+mutiplication_table_upto_20(8);
         
     }
     
