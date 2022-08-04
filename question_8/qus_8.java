b5e5158e1addfd095705e42af7b3ef142841a9c0
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-04 14:09:41 +0530

    8th question added

--- question_8/qus_8.java
+++ question_8/qus_8.java
@@ -3,15 +3,24 @@ package question_8;
 
 public class qus_8 {
     public static void mutiplication_table_upto_20(int n)
-    {
-        for(int i=1;i<=20;i++)
+    {  int i;
+        for( i=1;i<=20;i++)
         {
-            System.out.println("1X"=);
+            int total=i*n;
+            System.out.println(i+"x"+n+"="+total);
         }
     }
     
     public static void main(String[] args) {
 mutiplication_table_upto_20(8);
+System.out.println("Next input :");
+mutiplication_table_upto_20(9);
+System.out.println("Next input :");
+mutiplication_table_upto_20(10);
+System.out.println("Next input :");
+mutiplication_table_upto_20(5);
+
+
         
     }
     
