0e006058c0aab8224fee1b0d761c492f266ef914
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-10 05:31:38 +0530

    Add the other question

--- question_6/qus_6.java
+++ question_6/qus_6.java
@@ -1,6 +1,11 @@
 package question_6;
 
 public class qus_6 {
+    public static void add(int a,int b)
+    {
+          int c=a+b;
+          System.out.println(c);
+    }
     public static void m_n_Odd_number(int m,int n)
     {
         if(m<n)
@@ -21,12 +26,14 @@ public class qus_6 {
 
     }
     public static void main(String[] args) {
+        qus_6 obj=new qus_6();
         
         m_n_Odd_number(67, 89);
         System.out.println("Next input :");
         m_n_Odd_number(67, 67);
         System.out.println("Next input :");
         m_n_Odd_number(23, 56);
+        add(6,7);
 
     }
     
