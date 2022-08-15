7f631a0822b82aab323b4233d7828c31d5c16ebb
Ramkumar Selvaraj <ramkumar.iconao@gmail.com>
2022-08-15 14:34:04 +0530

    Add the other questions

--- question_18/qus_18.java
+++ question_18/qus_18.java
@@ -1,8 +1,72 @@
 package question_18;
 
+import java.util.Scanner;
+
 public class qus_18 {
+    
     public static void main(String[] args) {
         
+        String choice="";
+        Scanner sc=new Scanner(System.in);
+            
+        do{
+            
+            System.out.println("Enter the your input:");
+            int n=sc.nextInt();
+
+            if(n==1)
+            {
+                System.out.println("Enter the  number1:");
+                int a=sc.nextInt();
+                System.out.println("Enter the  number2:");
+                int b=sc.nextInt();
+                int c=a+b;
+                System.out.println("Add of two numbers is:"+c);
+            }
+            else if(n==2)
+            {
+                System.out.println("Enter the  number1:");
+                int a=sc.nextInt();
+                System.out.println("Enter the  number2:");
+                int b=sc.nextInt();
+                int c=a-b;
+                System.out.println("subraction  of two numbers is:"+c);
+            }
+            else if(n==3)
+            {
+                System.out.println("Enter the  number1:");
+                int a=sc.nextInt();
+                System.out.println("Enter the  number2:");
+                int b=sc.nextInt();
+                int c=a*b;
+                System.out.println("Multiplication   of two numbers is:"+c);
+            }
+            else if(n==4)
+            {
+                System.out.println("Enter the  number1:");
+                int a=sc.nextInt();
+                System.out.println("Enter the  number2:");
+                int b=sc.nextInt();
+                int c=a/b;
+                System.out.println("Division    of two numbers is:"+c);
+            }
+            else if(n==5)
+            {
+                System.out.println("Enter the  number1:");
+                int a=sc.nextInt();
+                System.out.println("Enter the  number2:");
+                int b=sc.nextInt();
+                int c=a%b;
+                System.out.println("Modoul Division   of two numbers is:"+c);
+            }
+            else{
+                System.out.println("Invalid number:");
+            }
+            System.out.println("Enter the Your choice: Yes or no");
+            
+               choice=sc.nextLine();
+            
+        }while(choice=="Yes");
     }
     
 }
--- question_19/qus_19.java
+++ question_19/qus_19.java
@@ -1,8 +1,45 @@
 package question_19;
 
+import java.util.Scanner;
+
 public class qus_19 {
+    public static void RPS(int n)
+    {
+        Scanner sc=new Scanner(System.in);
+        double b,a;
+        int AI=0,Human=0;
+        for(int i=1;i<n;i++)
+        {
+            System.out.println("Enter the your input number:");
+           a=sc.nextInt();
+           b= Math.random();
+           if(a>b)
+           {
+             AI++;
+           }
+           Human++;
+           if(AI>2)
+           break;
+           if (Human>2)
+           break;
+        }
+        if(AI>Human)
+        {
+            System.out.println("Human win ");
+        }
+        else if(AI==Human)
+        {
+            System.out.println("Draw");
+        }
+        else
+        {
+            System.out.println("AI win");
+        }
+        
+    }
     public static void main(String[] args) {
         
+        RPS(5);
     }
     
 }
